335dbb8ba6d1086fed3a67fead3113cabccf23fb
Mike Warot <chezmike@gmail.com>
2017-03-09 10:14:21 -0600

    added a button, starting to learn how events work

--- app/src/main/java/com/warot/android/bitgrid/MainActivity.java
+++ app/src/main/java/com/warot/android/bitgrid/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +47,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void btnTestClicked(View v) {
+
+        // a Toast is a message that goes away on its own, leaned about it at
+        // https://www.youtube.com/watch?v=bIZzbsARVbc
+        // 8:17 into the video
+
+        Toast.makeText(this, "Button Clicked",Toast.LENGTH_LONG).show();
+
     }
 }
